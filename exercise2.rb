c2421d7c52d4acb90feda5e1ed12d455302eb345
nickrdandrea <nickrdandrea@gmail.com>
2017-01-17 12:16:00 -0500

    Complete exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,10 @@
+# Multiply the dinner total by 1 + the tip percentage in decimal form
+puts 55 * 1.15
+
+#.to_i converts the string into an integer with a value of 0
+puts 5 + "Hello".to_i
+
+puts "The result of 45628 multiplied by 7839 is #{45628 * 7839}."
+
+# The results is true.
+puts (10 < 20 && 30 < 20) || !(10 == 11)
