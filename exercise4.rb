e0547dfdbe26f861811bb604c1374179ca888376
nickrdandrea <nickrdandrea@gmail.com>
2017-01-17 17:50:46 -0500

    Add comments to exercise 4

--- exercise4.rb
+++ exercise4.rb
@@ -1,10 +1,15 @@
-x = 1
+n = 1
 
+#loop 100 times
 100.times do
   s = ""
-  s.concat("Bit") if x%3 == 0
-  s.concat("Maker") if x%5 == 0
-  puts x if s == ""
+  #check divisibility of number and add appropriate strings
+  s.concat("Bit") if n%3 == 0
+  s.concat("Maker") if n%5 == 0
+  #print the number if it is not divisble by 3 or 5
+  puts n if s == ""
+  #print Fizz/Buzz string if number is divisible
   puts s unless s == ""
-  x += 1
+  #increment number
+  n += 1
 end
