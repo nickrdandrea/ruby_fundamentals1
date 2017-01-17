4288c221b2a3c82c6b9298e20bc3f900d0a2e58f
nickrdandrea <nickrdandrea@gmail.com>
2017-01-17 12:44:13 -0500

    Complete exercise 3

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,15 @@
+#ask the user their name and get input
 puts "What is your name?"
+
+name = gets.chomp
+
+#display name
+puts "Hi #{name}!"
+
+#ask the user their age and get input
+puts "How old are you?"
+
+age = gets.chomp
+
+#display birth year, may be incorrect based on month
+puts "You were born in ~#{Time.now.year - age.to_i}."
